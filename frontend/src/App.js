7739edc338ef04d05f1c55492a8f4496a0e5715c
Duncan Carbonnier <carbonnierduncan@gmail.com>
2022-03-11 10:28:32 +0100

    Frontend Home page
    home page's link created
    post's display done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Home from './pages/Home';
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className='navBar'>
+      <Link to="/" className='link'> Home Page</Link>
     </div>
+    <Switch>
+      <Route exact path="/"  component={Home}/>
+    </Switch>
+  </Router>
   );
 }
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {useHistory} from "react-router-dom"; 
+
+function Home() {
+    let history = useHistory();
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3002/posts").then((response) => {
+        setListOfPosts(response.data);
+    });
+    }, []);
+
+
+  return (
+    <div className="App">
+      {listOfPosts.map((value, key) => { 
+        return (
+        <div className="post" onClick={() => history.push(`/post/${value.id}`)}>
+            <div className="postHeader"> 
+              <div className="username">{value.username}</div>
+              <div className="title">{value.title}</div> 
+            </div>
+            <div className="body"> {value.postText}</div>
+        </div>
+        );
+        })}
+    </div>
+  )
+}
+
+export default Home
