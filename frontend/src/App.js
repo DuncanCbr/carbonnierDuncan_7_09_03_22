1aa48913108639af511b4d44b9b0970dfed64c44
Duncan Carbonnier <carbonnierduncan@gmail.com>
2022-03-11 10:34:26 +0100

    Create post page
    CPP's link created
    CPP done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Home from './pages/Home';
+import createpost from "./pages/Createpost";
 
 
 
@@ -9,10 +10,12 @@ function App() {
   return (
     <Router>
     <div className='navBar'>
+      <Link to="/createpost" className='link'> creat a post</Link>
       <Link to="/" className='link'> Home Page</Link>
     </div>
     <Switch>
       <Route exact path="/"  component={Home}/>
+      <Route exact path="/createpost"  component={createpost}/>
     </Switch>
   </Router>
   );
---
+++ frontend/src/pages/Createpost.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import {useHistory} from "react-router-dom"; 
+
+function Createpost() {
+
+  let history = useHistory();
+
+    const initialValues = {
+      title: "",
+      postText: "",
+      username: "",
+    }
+
+    const validationSchema = Yup.object().shape({
+      title: Yup.string().required(),
+      postText: Yup.string().required(),
+      username: Yup.string().min(3).max(15).required(),
+    })
+
+    const onSubmit = (data) => {
+      axios.post("http://localhost:3002/posts", data).then((response) => {
+        history.push("/");
+    });
+    }
+  return (
+    <div className='creatPostPage App'> 
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+            <Form className='form'>
+                <label className="fieldStyle"><span>Name </span>
+                <Field className="inputForm" id='inputCreatPost' name="postName" placeholder="(ex. John...)"/>
+                <ErrorMessage name="name" component="span" className='msgError' />
+                </label>
+                <label className="fieldStyle"><span>Title </span>
+                <Field className="inputForm" id='inputCreatPost' name="title" placeholder="(ex. Story Time...)"/>
+                <ErrorMessage name="title" component="span" className='msgError' />
+                </label>
+                <label className="fieldStyle"><span>Text</span>
+                <Field className="inputForm inputText" id='inputCreatPost' name="postText" placeholder="(ex. I live in paris...)"/>
+                <ErrorMessage name="postText" component="span" className='msgError' />
+                </label>
+                <button className="submitButton"type='submit'>Create your post</button>
+            </Form>
+        </Formik>
+    </div>
+  )
+}
+
+export default Createpost
