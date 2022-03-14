cefb8df96079801a067b89b987dcb08ff7dbc760
Duncan Carbonnier <carbonnierduncan@gmail.com>
2022-03-14 17:56:12 +0100

    frontend login
    login is now possible

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from "react";
+import axios from 'axios';
 
-function login() {
+function Login() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const Login = () => {
+    const data = {username: username, password: password};
+    axios.post("http://localhost:3002/auth/login", data).then((response) => {
+      console.log(response.data);
+    });
+  };
   return (
-    <div>login</div>
+    <div>
+      <input type="text" onChange={(event) => {setUsername(event.target.value)}}></input>
+      <input type="password" onChange={(event) => {setPassword(event.target.value)}}></input>
+      <button onClick={Login}>Login</button>
+    </div>
   )
 }
 
-export default login
+export default Login
